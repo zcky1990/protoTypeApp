5016f1be45addf5f481d2608b70d50b5699e8ebc
Soeltan Zaki Rizaldy <soeltanzaki.rizaldy@photoninfotech.net>
2016-10-25 18:35:24 +0700

    training lay outing

--- JavaFXApplication2/src/javafxapplication2/JavaFXApplication2.java
+++ JavaFXApplication2/src/javafxapplication2/JavaFXApplication2.java
@@ -10,7 +10,12 @@ import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.layout.Border;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 /**
@@ -22,20 +27,52 @@ public class JavaFXApplication2 extends Application {
     @Override
     public void start(Stage primaryStage) {
         Button btn = new Button();
-        btn.setText("Say 'Hello World'");
-        btn.setOnAction(new EventHandler<ActionEvent>() {
-            
+        
+        
+        //try layout
+        StackPane root = new StackPane();
+        root.getChildren().add(btn);
+        BorderPane border = new BorderPane();
+        BorderPane innerBorder = new BorderPane();
+        
+        //kotak Vertical
+        VBox vboxbot = new VBox();
+        vboxbot.getStyleClass().add("vbox");
+        innerBorder.setBottom(vboxbot);
+        
+        VBox vboxtop = new VBox();
+        vboxtop.getStyleClass().add("vbox");
+        innerBorder.setTop(vboxtop);
+        border.setRight(innerBorder);
+        
+        //kotak horizontal
+        HBox hbox = new HBox();
+        hbox.getStyleClass().add("hbox");
+        border.setLeft(hbox);
+        
+        //button
+        Button buttonCurrent = new Button("Current");
+        buttonCurrent.setPrefSize(100, 20);
+        buttonCurrent.setText("Say 'Hello World'");
+        
+        buttonCurrent.setOnAction(new EventHandler<ActionEvent>() {
             @Override
             public void handle(ActionEvent event) {
-                System.out.println("Hello World!");
+                vboxtop.getChildren().add(new Text("asdw"));
             }
         });
         
-        StackPane root = new StackPane();
-        root.getChildren().add(btn);
+        //text
+        Text t = new Text();
+        t.setText("This is a text sample");
         
-        Scene scene = new Scene(root, 300, 250);
+        hbox.getChildren().add(buttonCurrent);
+        hbox.getChildren().add(t);
         
+        Scene scene = new Scene(border);
+        scene.getStylesheets().add("css/layout.css");
+        
+        //add scene to stage
         primaryStage.setTitle("Hello World!");
         primaryStage.setScene(scene);
         primaryStage.show();
